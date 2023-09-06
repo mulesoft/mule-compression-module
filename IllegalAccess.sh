ad774d78c85f98a7a3536602820a31dd0bcba4d0
fglova <fglova@mulesoft.com>
2023-09-06 08:56:25 +0200

    run IllegalAccess.sh file and will see the NPE in junit tests

---
+++ IllegalAccess.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+RUNTIME_VERSION=4.4.0-20230522
+MUNIT_JVM=$(/usr/libexec/java_home -v 11.0.20)/bin/java
+
+# shellcheck disable=SC2155
+export JAVA_HOME=$(/usr/libexec/java_home -v 11.0.20)
+mvn clean
+mkdir target
+mvn verify \
+    -DruntimeProduct=MULE_EE \
+    -Dmule.disableSdkComponentIgnore=true \
+    -Dmunit.test.timeout=1200000 \
+    -DruntimeVersion=$RUNTIME_VERSION \
+    -Dmunit.jvm="$MUNIT_JVM" \
+    -Dmtf.javaopts="--illegal-access=warn" -e > ./target/test.log
+cat ./target/test.log | grep "WARNING: Illegal reflective access by" > ./target/illegal-access.log
