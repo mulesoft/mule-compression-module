a4772c7deb14ae17e31ecde10837f11337199b7d
Frantisek Glova <81289511+frantisekglova@users.noreply.github.com>
2023-12-01 12:39:15 -0300

    W-13822337 improve coverage and add Java 17 support (#133)
    (cherry picked from commit 750e98b1a3e4bf8696c2a8ac24776e064080c757)

--- src/main/java/org/mule/extension/compression/internal/CompressionExtension.java
+++ src/main/java/org/mule/extension/compression/internal/CompressionExtension.java
@@ -7,6 +7,10 @@
 package org.mule.extension.compression.internal;
 
 import static org.mule.runtime.api.metadata.MediaType.create;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_11;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_17;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
+
 import org.mule.extension.compression.api.strategy.ArchiverStrategy;
 import org.mule.extension.compression.api.strategy.CompressorStrategy;
 import org.mule.extension.compression.api.strategy.DecompressorStrategy;
@@ -23,12 +27,14 @@ import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Operations;
 import org.mule.runtime.extension.api.annotation.SubTypeMapping;
 import org.mule.runtime.extension.api.annotation.error.ErrorTypes;
+import org.mule.sdk.api.annotation.JavaVersionSupport;
 
 /**
  * A module which provides functionality for compressing and decompressing data.
  *
  * @since 1.0
  */
+@JavaVersionSupport({JAVA_8, JAVA_11, JAVA_17})
 @SubTypeMapping(baseType = CompressorStrategy.class, subTypes = {GzipCompressorStrategy.class, ZipCompressorStrategy.class})
 @SubTypeMapping(baseType = DecompressorStrategy.class, subTypes = {GzipDecompressorStrategy.class, ZipDecompressorStrategy.class})
 @SubTypeMapping(baseType = ExtractorStrategy.class, subTypes = {ZipExtractorStrategy.class})
